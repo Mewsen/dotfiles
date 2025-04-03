4f8029020e23b428f2709ed5b033591accdc7958
Michael Tews <michael@tews.dev>
2025-04-03 19:18:54 +0200

    editor: removed <c-t> keybind and added copilot
    Signed-off-by: Michael Tews <michael@tews.dev>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -77,8 +77,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
-vim.keymap.set('n', '<c-t>', '<cmd>ToggleTerm<CR>', { desc = 'Toggle Terminal' })
-
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
@@ -886,6 +884,9 @@ require('lazy').setup({
       vim.g.adwaita_transparent = true -- makes the background transparent
       vim.cmd 'colorscheme adwaita'
     end,
+  },
+  {
+    'github/copilot.vim',
   },
   {
     'echasnovski/mini.move',
